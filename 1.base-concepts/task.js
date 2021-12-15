6e6cd470d9ef9df78ce9b5c7644be277f427df1a
Aerofly <aerofly1@uandex.ru>
2021-12-15 15:20:13 +0300

    Задача 1 исполнена

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+
+  const dis = b ** 2 - 4 * a * c;
+  let x2 = (-b - Math.sqrt(dis)) / (2 * a);
+  let x1 = (-b + Math.sqrt(dis)) / (2 * a);
+  if (dis < 0) {
+    arr = [];
+  } else if (dis == 0) {
+    arr = [x1];
+  } else {
+    arr = [x1, x2];
+  }
+
   return arr; // array
 }
 
